25225dffe024b3bf438e10365111932f3030fb3e
Corbin Champion <corbinlc@gmail.com>
2018-09-19 13:24:29 -0700

    back out some unrelated changes

--- src/syscall/exit.c
+++ src/syscall/exit.c
@@ -137,12 +137,8 @@ void translate_syscall_exit(Tracee *tracee)
 		max_size  = peek_reg(tracee, MODIFIED, SYSARG_6);
 
 		status = translate_socketcall_exit(tracee, sock_addr, size_addr, max_size);
-		if (status < 0) {
-			if (status == -EFAULT) {
-				status = 0;
-			}
+		if (status < 0)
 			break;
-		}
 
 		/* Don't overwrite the syscall result.  */
 		goto end;
@@ -218,12 +214,8 @@ void translate_syscall_exit(Tracee *tracee)
 		max_size  = peek_reg(tracee, MODIFIED, SYSARG_6);
 
 		status = translate_socketcall_exit(tracee, sock_addr, size_addr, max_size);
-		if (status < 0) {
-			if (status == -EFAULT) {
-				status = 0;
-			}
+		if (status < 0)
 			break;
-		}
 
 		/* Don't overwrite the syscall result.  */
 		goto end;
