dcc74e9b1975b6b48e33a6ffe3e1da8c0b4bce71
Corbin Champion <corbinlc@gmail.com>
2018-12-04 16:51:19 -0800

    handle chroot for android 8+

--- src/extension/fake_id0/chroot.h
+++ src/extension/fake_id0/chroot.h
@@ -5,5 +5,6 @@
 #include "extension/fake_id0/config.h"
 
 int handle_chroot_exit_end(Tracee *tracee, Config *config);
+int handle_chroot_sigsys(Tracee *tracee, Config *config);
 
 #endif /* FAKE_ID0_CHROOT_H */
